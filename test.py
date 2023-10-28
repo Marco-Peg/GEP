bd50b38646a7be27636dfc8cedfa5be24289b164
Clementine Domine <clementine.domine98@gmail.com>
2023-10-28 12:48:12 +0100

    Update test.py

--- test.py
+++ test.py
@@ -14,17 +14,6 @@ from Utils.dataset import datasetCreator
 from Utils.losses import metrics_dict
 from Utils.train_utils import run_epoch, collate_None
 
-
-def run_DiffNet(model, data):
-    preds = model(data["ab_feats"], data["ab_massvec"], data["ag_feats"], data["ag_massvec"],
-                  L1=data["ab_L"], evals1=data["ab_evals"], evecs1=data["ab_evecs"], gradX1=data["ab_gradX"],
-                  gradY1=data["ab_gradY"], faces1=data.get("ab_faces", None),
-                  L2=data["ag_L"], evals2=data["ag_evals"], evecs2=data["ag_evecs"], gradX2=data["ag_gradX"],
-                  gradY2=data["ag_gradY"], faces2=data.get("ag_faces", None)).cpu()
-    if preds.dim() < 3:
-        preds = preds.view(1, -1, 1)  # batch x n_points x 1
-    return preds
-
 def parse_params():
     parser = argparse.ArgumentParser(description='Train diffusionNet on AbAg prediction.')
     parser.add_argument('-p', '--params-files', dest='params_file', default='Param_runs/diffNet_mesh_split.json',
@@ -39,3 +28,13 @@ def parse_params():
     params["seed"] = args.seed
 
     return args, params
+
+if __name__ == "__main__":
+    args, params = parse_params()
+    dataset_name = params["dataset_name"]
+    # system things
+    if torch.cuda.is_available():
+        device = params["device"]
+    else:
+        print("cuda not available, using cpu as deafult")
+        device = torch.device("cpu")
